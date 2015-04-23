4c4972876509bfb0764f8ba9081341255c55b656
Henry J Schmale <hschmale16@gmail.com>
2015-04-23 19:18:40 -0400

    Rm File in Script

--- assembleFrames.sh
+++ assembleFrames.sh
@@ -9,7 +9,8 @@ frat_glob=out/frame%010d.jpg
 # $1 - file glob pattern
 # $2 - output file name
 assembImg2Mp4(){
-    ffmpeg -framerate 30 -i $1 -c:v libx264 -vf fps=30 $2
+    if [ -e $2 ] ; then rm -f $2 ; fi
+    ffmpeg -framerate 60 -i $1 -c:v libx264 -vf fps=60 $2
 }
 
 assembImg2Mp4 $frat_glob $final_name
